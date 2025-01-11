51e9a3ac5a71762d641564da658bf50fb4f6a827
JiazhenZhao02091 <873694994@qq.com>
2025-01-11 17:29:39 +0800

    完成find

---
+++ user/find.c
@@ -0,0 +1,122 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+// 返回path路径指向的文件名称
+char *fmtname(char *path);
+int find(char *path, char *name, int flag);
+// find dir name
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        fprintf(2, "Usage: find dir name\n");
+        exit(1);
+    }
+    // find dir name
+    char *dir = argv[1];  // dir
+    char *name = argv[2]; // name
+
+    if (find(dir, name, 0) == -1)
+        printf("未找到.\n");
+
+    exit(0);
+}
+int find(char *path, char *name, int flag) // flag == 0 --> 目录为 ./..
+{
+
+    int x = 0; // false
+    char buf[512], *p;
+    int fd;
+    struct dirent de; // inum、name
+    struct stat st;   // 文件信息：inode、大小、链接计数、文件类型
+
+    if ((fd = open(path, 0)) < 0) // 打开 path文件（普通文件 或者 目录文件）
+    {
+        fprintf(2, "ls: cannot open %s\n", path);
+        return -1;
+    }
+
+    if (fstat(fd, &st) < 0) // fstat系统调用获取打开文件的状态信息
+    {
+        fprintf(2, "ls: cannot stat %s\n", path);
+        close(fd);
+        return -1;
+    }
+    switch (st.type)
+    {
+
+    case T_FILE:
+        // printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+        printf("所输入目录名不正确%s\n", fmtname(path));
+        break;
+
+    case T_DIR:
+        // printf("T_DIR\n");
+        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+        {
+            printf("ls: path too long\n");
+            break;
+        }
+        strcpy(buf, path);
+        p = buf + strlen(buf);
+        *p++ = '/';
+
+        while (read(fd, &de, sizeof(de)) == sizeof(de)) // 每次读一个 de 结构体
+        {
+            if (de.inum == 0)
+                continue;
+
+            memmove(p, de.name, DIRSIZ); // dest src size // 移动de位置到p末尾，即buf末尾
+            p[DIRSIZ] = 0;
+
+            if (stat(buf, &st) < 0) // stat 函数通过文件路径来获取文件的状态信息
+            {
+                printf("ls: cannot stat %s\n", buf);
+                continue;
+            }
+
+            if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+                continue;
+
+            if (st.type == T_DIR)
+                find(buf, name, 1);
+            if (st.type == T_FILE)
+            {
+                if (strcmp(name, fmtname(buf)) == 0) // 比较文件名
+                {
+                    x = 1;
+                    printf("%s\n", buf);
+                }
+            }
+        }
+        break;
+    }
+    if (x > 0)
+    {
+        close(fd);
+        return 0;
+    }
+    close(fd);
+    return -1;
+}
+// 返回path路径指向的文件名称
+char *fmtname(char *path)
+{
+    static char buf[DIRSIZ + 1];
+    char *p;
+
+    // Find first character after last slash.
+    for (p = path + strlen(path); p >= path && *p != '/'; p--)
+        ;
+    p++;
+
+    // Return blank-padded name.
+    if (strlen(p) >= DIRSIZ)
+        return p;
+    memmove(buf, p, strlen(p));
+    // memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));    填充0
+    buf[strlen(p)] = 0;
+    return buf;
+}
--- user/ls.c
+++ user/ls.c
@@ -3,66 +3,75 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-char*
-fmtname(char *path)
+// 返回path指向的文件名称
+char *fmtname(char *path)
 {
-  static char buf[DIRSIZ+1];
+  static char buf[DIRSIZ + 1];
   char *p;
 
   // Find first character after last slash.
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+  for (p = path + strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
 
   // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
+  if (strlen(p) >= DIRSIZ)
     return p;
   memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
   return buf;
 }
 
-void
-ls(char *path)
+void ls(char *path)
 {
   char buf[512], *p;
   int fd;
-  struct dirent de;
-  struct stat st;
+  struct dirent de; // inum、name
+  struct stat st;   // 文件信息：inode、大小、链接计数、文件类型
 
-  if((fd = open(path, 0)) < 0){
+  if ((fd = open(path, 0)) < 0) // 打开 path文件
+  {
     fprintf(2, "ls: cannot open %s\n", path);
     return;
   }
 
-  if(fstat(fd, &st) < 0){
+  if (fstat(fd, &st) < 0) // fstat系统调用获取打开文件的状态信息
+  {
     fprintf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
-  switch(st.type){
+  switch (st.type)
+  {
   case T_FILE:
     printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
     break;
 
   case T_DIR:
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+    {
       printf("ls: path too long\n");
       break;
     }
     strcpy(buf, path);
-    p = buf+strlen(buf);
+    p = buf + strlen(buf);
     *p++ = '/';
-    while(read(fd, &de, sizeof(de)) == sizeof(de)){
-      if(de.inum == 0)
+
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) // 每次读一个 de 大小
+    {
+      if (de.inum == 0)
         continue;
-      memmove(p, de.name, DIRSIZ);
+
+      memmove(p, de.name, DIRSIZ); // dest src size // 移动de位置？
       p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
+
+      if (stat(buf, &st) < 0) // stat 函数通过文件路径来获取文件的状态信息
+      {
         printf("ls: cannot stat %s\n", buf);
         continue;
       }
+
       printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
     }
     break;
@@ -70,16 +79,17 @@ ls(char *path)
   close(fd);
 }
 
-int
-main(int argc, char *argv[])
+// ls
+int main(int argc, char *argv[])
 {
   int i;
 
-  if(argc < 2){
-    ls(".");
+  if (argc < 2) // 0 1
+  {
+    ls("."); // 默认 只有1个参数 此时 为 ls
     exit(0);
   }
-  for(i=1; i<argc; i++)
+  for (i = 1; i < argc; i++)
     ls(argv[i]);
   exit(0);
 }
